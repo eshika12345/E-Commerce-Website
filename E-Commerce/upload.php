58e0fd3a6a7ba1c025a8b4a26f2ca28cac33c465
eshika12345 <98465602+eshika12345@users.noreply.github.com>
2022-10-27 12:05:46 +0530

    Delete upload.php

--- E-Commerce/upload.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-<form action="uploadprocess.php" method="post" enctype="multipart/form-data">
-Select image to upload :
-<input type="file" name="file"/>
-<button type="submit" name="upload">Upload</button>
-</form>
-</body>
-</html>
